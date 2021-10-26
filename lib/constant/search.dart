ccaa538ddf6e60a93f3d8315dfd72a22dc2c878f
Faruq <akindoyinfaruq@gmail.com>
2021-10-26 12:30:41 +0100

    more_search images

--- lib/constant/search.dart
+++ lib/constant/search.dart
@@ -27,5 +27,16 @@ List searchImages = [
    "https://images.unsplash.com/photo-1506901437675-cde80ff9c746?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
    "https://images.unsplash.com/photo-1500336624523-d727130c3328?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
    "https://images.unsplash.com/photo-1504933350103-e840ede978d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-   "https://images.unsplash.com/photo-1467632499275-7a693a761056?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+   "https://images.unsplash.com/photo-1467632499275-7a693a761056?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+   "https://i.pinimg.com/236x/2a/4b/f2/2a4bf22f150ca0a0bf37ce4ed75ab318.jpg",
+   "https://i.pinimg.com/236x/58/4d/34/584d34d201af09f2690cd079f4eefb32.jpg",
+   "https://i.pinimg.com/236x/80/8f/9b/808f9b066a801fe8819791f2467d06a1.jpg",
+   "https://i.pinimg.com/236x/d2/22/66/d22266f0ce0c918d1f9a0de253074341.jpg",
+   "https://i.pinimg.com/236x/fd/c9/05/fdc905b1fa8492a6079b75deed52b82f.jpg",
+   "https://i.pinimg.com/236x/9f/7e/a2/9f7ea289e14d348b6212de8ade1a083a.jpg",
+   
+
+
+
+
 ];
